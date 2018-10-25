0ee2f1c9ecbaa7066e69d47ba5b9ee15b7454482
Jeffrey Henderson <jeffrey.henderson17@gmail.com>
2018-10-24 23:11:04 -0400

    adds an audio example to the demo.

--- application/frontend/src/app/demo/demo.component.ts
+++ application/frontend/src/app/demo/demo.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
-  templateUrl: './demo.component.html'
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
 })
 export class DemoComponent implements OnInit {
   basicSentence: String;
