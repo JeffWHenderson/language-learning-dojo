524ae5688c887f3239c182c6059511f60cb5cfa7
Jeffrey Henderson <jeffrey.henderson17@gmail.com>
2018-11-14 21:35:36 -0500

    wires in the components in lesson module.

--- application/frontend/src/app/app.module.ts
+++ application/frontend/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
+import { LessonModule } from './lesson/lesson.module';
+import { VideoComponent } from './lesson/components/video/video.component';
+import { FlashcardsComponent } from './lesson/components/flashcards/flashcards.component';
+import { SentenceListComponent } from './lesson/components/sentence-list/sentence-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DemoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LessonModule
+
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
--- application/frontend/src/app/lesson/lesson.module.ts
+++ application/frontend/src/app/lesson/lesson.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SentenceListComponent } from './components/sentence-list/sentence-list.component';
+import { FlashcardsComponent } from './components/flashcards/flashcards.component';
+import { VideoComponent } from './components/video/video.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    VideoComponent,
+    SentenceListComponent,
+    FlashcardsComponent
+  ],
+  exports: [
+    VideoComponent,
+    FlashcardsComponent,
+    SentenceListComponent
+  ]
 })
 export class LessonModule { }
