802db8ac9062fe7d667caf59ed13e8f3c2bc1dda
Jeffrey Henderson <jeffrey.henderson17@gmail.com>
2018-11-14 22:21:57 -0500

    adds welcome page, route to Demo, and separate component routes.

--- application/frontend/src/app/app.module.ts
+++ application/frontend/src/app/app.module.ts
@@ -7,10 +7,14 @@ import { VideoComponent } from './lesson/components/video/video.component';
 import { FlashcardsComponent } from './lesson/components/flashcards/flashcards.component';
 import { SentenceListComponent } from './lesson/components/sentence-list/sentence-list.component';
 import { AppRoutingModule } from './routes/app-routing.module';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
--- application/frontend/src/app/routes/app-routing.module.ts
+++ application/frontend/src/app/routes/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { FlashcardsComponent } from '../lesson/components/flashcards/flashcards.component';
 import { VideoComponent } from '../lesson/components/video/video.component';
 import { SentenceListComponent } from '../lesson/components/sentence-list/sentence-list.component';
+import { WelcomePageComponent } from '../welcome-page/welcome-page.component';
+import { DemoComponent } from '../demo/demo.component';
 
 const routes: Routes = [
-    // { path: '', component: FlashcardsComponent },d
+    { path: '', component: WelcomePageComponent },
     { path: 'flashcards', component: FlashcardsComponent },
     { path: 'video', component: VideoComponent },
     { path: 'sentences', component: SentenceListComponent },
+    { path: 'demo', component: DemoComponent }
 ];
 
 @NgModule({
