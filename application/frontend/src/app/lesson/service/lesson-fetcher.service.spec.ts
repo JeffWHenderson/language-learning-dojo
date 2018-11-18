43f74f0e8ee9da3ff0709358d9e114dea4fbdc76
Jeffrey Henderson <jeffrey.henderson17@gmail.com>
2018-11-18 10:10:51 -0500

    adds http get call to lesson fetcher service.

--- application/frontend/src/app/lesson/service/lesson-fetcher.service.spec.ts
+++ application/frontend/src/app/lesson/service/lesson-fetcher.service.spec.ts
@@ -1,10 +1,12 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { getTestBed, TestBed, inject } from '@angular/core/testing';
 import { LessonFetcherService } from './lesson-fetcher.service';
+import { Subscription } from 'rxjs';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('LessonFetcherService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [LessonFetcherService]
     });
   });
@@ -12,4 +14,24 @@ describe('LessonFetcherService', () => {
   it('should be created', inject([LessonFetcherService], (service: LessonFetcherService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should search by lesson Number', () => {
+    let testBed: TestBed = getTestBed();
+    let lessonFetcherService: LessonFetcherService = testBed.get(LessonFetcherService);
+    let httpMock = testBed.get(HttpTestingController);
+
+    // TODO: MOVE SUBSCRIPTIONS CODE BEOLE TO COMPONENTS THAT SUBSCRIBE
+    // let subscriptions: Subscription = new Subscription();
+    
+    // subscriptions.add(lessonFetcherService.subscribableLessonData$.subscribe(sentence => {
+    //     console.log(sentence)
+    // }));
+    
+    lessonFetcherService.getLesson("1");
+
+    const request = httpMock.expectOne('baseURL/api/1');
+    expect(request.request.method).toBe('GET');
+    // when http call is done, mock return (happy, unhappy)
+    // Happy expect an update subject to be updated async
+  })
 });
--- application/frontend/src/app/lesson/service/lesson-fetcher.service.ts
+++ application/frontend/src/app/lesson/service/lesson-fetcher.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LessonFetcherService {
+  subscribableLessonData$: Observable<any>;
+  private updateSubject: Subject<any> = new Subject();
 
-  constructor() { }
+  // test code
+  private counter: number = 0;
+  private sentences: String[] = ["sentence 1", "sentence 2", "sentence 3"]
+
+  constructor(private http: HttpClient) {
+    this.subscribableLessonData$ = this.updateSubject.asObservable();
+   }
+
+  getLesson(lessonNumber: String): void {
+    // TODO: make an http Call to make the object
+    this.updateSubject.next(this.sentences[this.counter])
+
+    this.http.get("baseURL/api/" + lessonNumber).subscribe(result => {
+      console.log(result);
+    })
+    this.counter +=1 
+  }
 }
